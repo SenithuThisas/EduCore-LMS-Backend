2b95b337eab8b5a57d64843563eeb88e657c1151
Senithu Thisas <st20306044@outlook.cardiffmet.ac.uk>
2025-06-18 23:39:38 +0530

    Add bcrypt library and update nodemon version

---
+++ backend/hash.js
@@ -0,0 +1,8 @@
+// const bcrypt = require('bcrypt');
+// const password = 'alice2025';
+// const saltRounds = 10;
+
+// bcrypt.hash(password, saltRounds, function(err, hash) {
+//   if (err) throw err;
+//   console.log(hash); // Copy this hash and use it in your database
+// });
