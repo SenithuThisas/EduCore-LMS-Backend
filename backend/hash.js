8764d1d35aa6fbb57184f8f8bd5ba73e11944958
SenithuThisas <st20306044@outlook.cardiffmet.ac.uk>
2025-11-19 12:59:39 +0530

    Remove unused backend files for improved code clarity

--- backend/hash.js
+++
@@ -1,8 +0,0 @@
-// const bcrypt = require('bcrypt');
-// const password = 'mike2025';
-// const saltRounds = 10;
-
-// bcrypt.hash(password, saltRounds, function(err, hash) {
-//   if (err) throw err;
-//   console.log(hash); // Copy this hash and use it in your database
-// });
